6959eb295b4290f39ac071e6532445db652b0920
nikhil3009 <sainikhil3009@gmail.com>
2024-05-09 20:16:26 -0400

    Initial commit

--- src/controllers/info-controller.js
+++ src/controllers/info-controller.js
@@ -5,7 +5,7 @@ const { StatusCodes } = require('http-status-codes');
 const info = (req, res) => {
 	return res.status(StatusCodes.OK).json({
 		success: true,
-		message: 'API is live and running',
+		message: 'API is live',
 		error: {},
 		data: {},
 	});
