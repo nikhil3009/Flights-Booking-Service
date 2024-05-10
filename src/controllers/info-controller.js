ef60f3b10dee8f810448c82469519dd6578ac012
nikhil3009 <sainikhil3009@gmail.com>
2024-05-09 20:15:11 -0400

    Basic Setup

--- src/controllers/info-controller.js
+++ src/controllers/info-controller.js
@@ -1,14 +1,16 @@
+/** @format */
+
 const { StatusCodes } = require('http-status-codes');
 
 const info = (req, res) => {
-    return res.status(StatusCodes.OK).json({
-        success: true,
-        message: 'API is live',
-        error: {},
-        data: {},
-    });
-}
+	return res.status(StatusCodes.OK).json({
+		success: true,
+		message: 'API is live and running',
+		error: {},
+		data: {},
+	});
+};
 
 module.exports = {
-    info
-}
+	info,
+};
